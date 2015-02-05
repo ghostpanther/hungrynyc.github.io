95a53b6fbd00e36ab7bc652f4fd246dc6d19697c
zmess24 <zac.messinger@gmail.com>
2015-02-05 11:17:57 -0500

    added javascript to test hit to back end

---
+++ js/hungry.js
@@ -0,0 +1,12 @@
+console.log('JS and JQ loaded!')
+
+function test(){
+	$.ajax('https://powerful-falls-2281.herokuapp.com/' {
+		type: 'GET',
+		dataType: 'json'
+	}.done(function(data){
+		console.log(data)
+	});
+};
+
+test();
