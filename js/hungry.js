bf5c4ccde2ab85c4467fbd31c9507adab63b86fc
zmess24 <zac.messinger@gmail.com>
2015-02-05 11:20:57 -0500

    added ,

--- js/hungry.js
+++ js/hungry.js
@@ -1,7 +1,7 @@
 console.log('JS and JQ loaded!')
 
 function test(){
-	$.ajax('https://powerful-falls-2281.herokuapp.com/' {
+	$.ajax('https://powerful-falls-2281.herokuapp.com/', {
 		type: 'GET',
 		dataType: 'json'
 	}).done(function(data){
