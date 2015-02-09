52e9cffebaae812a6d13c9679421fbf7b8d3f28f
zmess24 <zac.messinger@gmail.com>
2015-02-09 17:17:35 -0500

    ?

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log('LINKED');
+
+$(function(){
+
+  $('form').on('submit', function(){
+    $.ajax({
+      url: 'http://127.0.0.1:3000/sessions',
+      type: 'POST',
+      data: { "email": $('.email')[0].value, "password": $('.password')[0].value},
+      // dataType: 'json'
+    })
+  });
+});
