7847849ec32e2febfadde4375f96685bbda66d2c
sjackson44 <sjackson44@gmail.com>
2015-02-09 18:52:20 -0500

    got sessions working

--- js/index.js
+++ js/index.js
@@ -2,18 +2,19 @@ console.log('LINKED');
 
 $(function(){
 
-  $('#login_form').on('submit', function(e){
+  $('#login-form').on('submit', function(e){
   	  	e.preventDefault()
   	var email = $(".login_email").val();
   	var pass = $(".login_pass").val();
   	console.log(email +" " + pass)
+
 	    $.ajax({
 	      url: 'http://127.0.0.1:3000/sessions',
 	      type: 'POST',
 	      data: { "email": email, "password": pass}
 	    }).done(function(data){
 	    	console.log(data);
-	    	// sessionStorage.setItem("","")
+	    	sessionStorage.setItem("","")
 	    })
 	  
 	});
