61fc0fb12662ecd835748e28114fb4f12a8ad890
amyyli <amyyli09@gmail.com>
2015-02-10 10:01:10 -0500

    fix signup variables

--- js/signup.js
+++ js/signup.js
@@ -3,8 +3,8 @@ console.log('LINKED');
 $(function(){
 
   $('#sign-up').on('submit', function(){
-    var fname = $("input[name='fname']").val()
-    var lname = $("input[name='lname']").val()
+    var first = $("input[name='first']").val()
+    var last = $("input[name='last']").val()
     var email = $("input[name='email']").val()
     var password = $("input[name='password']").val()
     $.ajax({
