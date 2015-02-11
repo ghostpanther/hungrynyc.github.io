45fcc8ba4029793e3089e2873fdf7aacec9fb24f
sjackson44 <sjackson44@gmail.com>
2015-02-10 20:59:44 -0500

    fixed the user sign up, sign in, and logout functionality

--- js/signup.js
+++ js/signup.js
@@ -2,21 +2,31 @@ console.log('signup.js');
 
 $(function(){
 
-  $('#sign-up').on('submit', function(){
+  $('#signup-form').on('submit', function(e){
+     e.preventDefault()
     var first = $("input[name='fname']").val()
     var last = $("input[name='lname']").val()
-    var email = $("input[name='email']").val()
-    var password = $("input[name='password']").val()
-    $.ajax({
-      url: 'http://127.0.0.1:3000/sessions',
+    var email = $("input[name='signup_email']").val()
+    var password = $("input[name='signup_password']").val()
+    
+   $.ajax({
+      url: 'http://127.0.0.1:3000/users',
       type: 'POST',
       data: { "f_name": first, "l_name": last, "email": email, "password": password },
       dataType: 'json'
     }).done(function(data){
-      console.log('You just signed up!')
-      console.log(data);
-      window.location.href="./dashboard.html";
-    });
+     
+      $.ajax({
+        url: 'http://127.0.0.1:3000/sessions',
+        type: 'POST',
+        data: { "email": email, "password": password}
+      }).done(function(data){
+        sessionStorage.setItem("token", data["key"]);
+        sessionStorage.setItem("id", data["id"]);
+        window.location.href = "./dashboard.html";
+      })
+    })
+
   });
 
 });
